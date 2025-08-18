19540ebf019f998bd960348637d72a42f353e016
Its-Maeae <mariusschmi1711@gmail.com>
2025-08-18 14:46:57 +0200

    feat: Korrigiere die Anzeige von Mineral-Details und verbessere das Modal-Verhalten

--- public/app.js
+++ public/app.js
@@ -319,12 +319,10 @@ function displayMinerals() {
     `).join('');
 }
 
-// Mineral-Details anzeigen - VERBESSERTE VERSION
+// Mineral-Details anzeigen - KORRIGIERTE VERSION
 async function showMineralDetails(id) {
     try {
-        // Erst alle anderen Modals schließen
-        closeShelfMineralsModal();
-        closeVitrineDetailModal();
+        // Diese Modals bleiben jetzt offen wenn man aus einem Regal kommt!
         
         const response = await fetch(`${API_BASE}/minerals/${id}`);
         
